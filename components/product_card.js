f879d4087d476f6a95f32119f0db78771bdc2fa5
Enc0re3gone <skalain12@gmail.com>
2023-05-14 01:55:01 +0400

    changed class name

--- components/product_card.js
+++ components/product_card.js
@@ -1,18 +1,18 @@
 import stringToHtml from "../plugins/stringToHtml.js"
 
 const createProductCard = ({ id, title, price, category, description, image }) => stringToHtml(`
-<div class="product_card" id="product_card-${id}">
-  <div class="product_card-image">
+<a class="product__card" id="product__card-${id}" href="/product/${id}">
+  <div class="product__card-image">
     <img src="${image}">
   </div>
-  <div class="product_card-body">
+  <div class="product__card-body">
     <h3>${title}</h3>
     <p>${description}</p>
-    <div class="product_card-body-price">
+    <div class="product__card-body-price">
       <h3>${price} руб.</h3>  
     </div>
   </div>
-</div>
+</a>
 `)
 
 
