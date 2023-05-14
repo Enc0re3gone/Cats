59980d991cbfab0e1736d1097cfd1ef8091ab3bb
Enc0re3gone <skalain12@gmail.com>
2023-05-14 21:18:14 +0400

    added button add

--- components/product_card.js
+++ components/product_card.js
@@ -1,18 +1,19 @@
 import stringToHtml from "../plugins/stringToHtml.js"
 
 const createProductCard = ({ id, title, price, category, description, image }) => stringToHtml(`
-<a class="product__card" id="product__card-${id}" href="/product/${id}">
-  <div class="product__card-image">
+<div class="product__card" id="product__card-${id}">
+  <a href="/product/${id}" class="product__card-image">
     <img src="${image}">
-  </div>
+  </a>
   <div class="product__card-body">
-    <h3>${title}</h3>
+    <h3><a href="/product/${id}">${title}</a></h3>
     <p>${description}</p>
     <div class="product__card-body-price">
+      <button>Добавить</button>
       <h3>${price} руб.</h3>  
     </div>
   </div>
-</a>
+</div>
 `)
 
 
