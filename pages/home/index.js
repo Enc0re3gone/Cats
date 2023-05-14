738e5f65dbc81a74ae35726c7aaf285909613d2d
Enc0re3gone <skalain12@gmail.com>
2023-05-14 21:22:56 +0400

    added event listener for add and remove item from cart

--- pages/home/index.js
+++ pages/home/index.js
@@ -7,6 +7,30 @@ const wrap = document.querySelector('article .wrap')
 
 document.addEventListener('getProducts', (e) => getProducts(e.detail))
 
+function setProductsForCart (id, action) {
+  const products = $localStorage.get('products') || []
+
+  if (action === 'add') {
+    products.push(id)
+  } else if (action === 'remove') {
+    products.splice(products.findIndex(product => product === id), 1)
+  }
+
+  $localStorage.set('products', products)
+  document.querySelector('header .cart').dataset.content = products.length > 9 ? '9+' : products.length
+}
+
+function productBtnHandler (id, btn) {
+  btn.classList.toggle('secondary')
+  if (btn.innerHTML === 'Добавить') {
+    setProductsForCart(id, 'add')
+    btn.innerHTML = 'Убрать'
+  } else {
+    btn.innerHTML = 'Добавить'
+    setProductsForCart(id, 'remove')
+  }
+}
+
 async function getProducts(query) {
   wrap.innerHTML = ''
   wrap.append(loader())
@@ -16,8 +40,18 @@ async function getProducts(query) {
       if (res.length) {
         document.querySelector('.wrap').innerHTML = ''
 
+        const productsFromStorage = $localStorage.get('products')
+
         res.forEach(product => {
           wrap.append(product_card(product))
+          const product_cardBtn = wrap.querySelector(`#product__card-${product.id} button`)
+
+          if (productsFromStorage.includes(product.id)) {
+            product_cardBtn.classList.add('secondary')
+            product_cardBtn.innerHTML = 'Убрать'
+          }
+
+          product_cardBtn.addEventListener('click', () => productBtnHandler(product.id, product_cardBtn))
         })
       } else {
         wrap.innerHTML = 'Список пуст'
